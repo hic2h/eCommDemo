08bafb537463cfe7a7573a30a0ce7939d9cdbb9d
mohamed.belmahi2008@gmail.com <Prince999GolD>
2016-11-26 16:05:24 +0000

    delete company.js/company.service.js

--- src/app/company/company.service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('company').factory('CompanyService', ['AuthApi', function (AuthApi) {
-  return {
-    getAuthenticatedCompany : function(login, pwd) {
-      return AuthApi.authenticate(login, pwd);
-    }
-  }
-}]);
